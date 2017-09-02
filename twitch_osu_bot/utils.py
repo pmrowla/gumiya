da088cb22398cbdd02998109f487da4924905ef6
Peter Rowlands <peter@pmrowla.com>
2017-09-02 13:41:55 +0900

    [twitch] use new session per api call
    fix #11

--- twitch_osu_bot/utils.py
+++ twitch_osu_bot/utils.py
@@ -17,40 +17,48 @@ class TwitchApi(object):
     GET_LIVE_STREAMS = '/'.join((API_BASE, 'streams'))
     GET_STREAM_BY_USER = '/'.join((GET_LIVE_STREAMS, '{channel_id}'))
 
-    def __init__(self):
-        self.session = requests.Session()
-        try:
-            app = SocialApp.objects.get(provider='twitch')
-        except SocialApp.DoesNotExist:
-            app = SocialApp.objects.create(name='twitch', provider='twitch')
-        self.session.headers.update({
-            'Accept': 'application/vnd.twitchtv.v5+json',
-            'Client-ID': app.client_id,
-        })
+    def __init__(self, headers={}):
+        if headers:
+            self.headers = headers
+        else:
+            try:
+                app = SocialApp.objects.get(provider='twitch')
+            except SocialApp.DoesNotExist:
+                app = SocialApp.objects.create(name='twitch', provider='twitch')
+            self.headers = {
+                'Accept': 'application/vnd.twitchtv.v5+json',
+                'Client-ID': app.client_id,
+            }
 
     @classmethod
     def from_user(cls, user):
-        api = cls()
+        headers = {}
         for token in SocialToken.objects.filter(account__user=user, account__provider='twitch'):
             if token.expires_at is None or token.expires_at > timezone.now():
-                api.session.headers.update({
+                headers = {
                     'Client-ID': token.app.client_id,
                     'Authorization': 'OAuth {}'.format(token.token),
-                })
-        return api
+                }
+        return cls(headers=headers)
 
     def _parse_timestamps(self, d, keys):
         for k in keys:
             if k in d:
                 d[k] = parse_datetime(d[k])
 
+    def _session(self):
+        s = requests.Session()
+        s.headers.update(self.headers)
+        return s
+
     def get_user(self):
         """Fetch details for the specified user from the Twitch API."""
-        r = self.session.get(self.GET_USER)
-        if r.status_code == 200:
-            twitch_user = r.json()
-            self._parse_timestamps(twitch_user, ['created_at', 'updated_at'])
-            return twitch_user
+        with self._session() as s:
+            r = s.get(self.GET_USER)
+            if r.status_code == 200:
+                twitch_user = r.json()
+                self._parse_timestamps(twitch_user, ['created_at', 'updated_at'])
+                return twitch_user
         return None
 
     def get_stream_by_user(self, twitch_user, stream_type=None):
@@ -59,10 +67,11 @@ class TwitchApi(object):
             params = {'stream_type': stream_type}
         else:
             params = {}
-        r = self.session.get(url, params=params)
-        if r.status_code == 200:
-            twitch_stream = r.json()
-            return twitch_stream.get('stream')
+        with self._session() as s:
+            r = s.get(url, params=params)
+            if r.status_code == 200:
+                twitch_stream = r.json()
+                return twitch_stream.get('stream')
         return None
 
     def get_live_streams(self, twitch_users=[], game=None, language=None, stream_type=None):
@@ -75,11 +84,12 @@ class TwitchApi(object):
         if stream_type in ['live', 'playlist', 'all']:
             params['stream_type'] = stream_type
         if not twitch_users:
-            r = self.session.get(url, params=params)
-            if r.status_code == 200:
-                twitch_streams = r.json()
-                if 'streams' in twitch_streams:
-                    return twitch_streams['streams']
+            with self._session() as s:
+                r = s.get(url, params=params)
+                if r.status_code == 200:
+                    twitch_streams = r.json()
+                    if 'streams' in twitch_streams:
+                        return twitch_streams['streams']
             return []
         else:
             # Twitch will only return a maximum of 100 results, if we need more
@@ -88,9 +98,10 @@ class TwitchApi(object):
             streams = []
             for channels in [twitch_users[i:i + 100] for i in range(0, len(twitch_users), 100)]:
                 params['channel'] = ','.join(map(lambda x: str(x.twitch_id), channels))
-                r = self.session.get(url, params=params)
-                if r.status_code == 200:
-                    twitch_streams = r.json()
-                    if 'streams' in twitch_streams:
-                        streams.extend(twitch_streams['streams'])
+                with self._session() as s:
+                    r = s.get(url, params=params)
+                    if r.status_code == 200:
+                        twitch_streams = r.json()
+                        if 'streams' in twitch_streams:
+                            streams.extend(twitch_streams['streams'])
             return streams
