08ed0783e4d9d740d23fd2478b609f5223e538d0
Peter Rowlands <peter@pmrowla.com>
2022-04-29 01:21:40 +0900

    django 3.2 updates

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,13 +8,15 @@ Local settings
 - Add Django Debug Toolbar
 - Add django-extensions as app
 """
+import socket
 
 from .base import *  # noqa
+from .base import env
 
 # DEBUG
 # ------------------------------------------------------------------------------
-DEBUG = env.bool("DJANGO_DEBUG", default=True)
-TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
+DEBUG = True
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2", "192.168.99.100"]
 
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
@@ -44,43 +46,28 @@ CACHES = {
     }
 }
 
+# whitenoise
+INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
+
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
-INSTALLED_APPS += [
-    "debug_toolbar",
-]
-
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2", "192.168.99.100"]
-ALLOWED_HOSTS = INTERNAL_IPS + ["localhost", "[::1]"]
-
-
-import os
-import socket
-
-# tricks to have debug toolbar when developing with docker
-if os.environ.get("USE_DOCKER") == "yes":
-    ip = socket.gethostbyname(socket.gethostname())
-    INTERNAL_IPS += [ip[:-1] + "1"]
-
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
 DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": [
-        "debug_toolbar.panels.redirects.RedirectsPanel",
-    ],
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
 
+# tricks to have debug toolbar when developing with docker
+if env("USE_DOCKER") == "yes":
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+
+ALLOWED_HOSTS = INTERNAL_IPS + ["localhost", "[::1]"]
+
 # django-extensions
 # ------------------------------------------------------------------------------
-INSTALLED_APPS += [
-    "django_extensions",
-]
-
-# TESTING
-# ------------------------------------------------------------------------------
-TEST_RUNNER = "django.test.runner.DiscoverRunner"
+INSTALLED_APPS += ["django_extensions"]  # noqa F405
 
 # Your local stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
